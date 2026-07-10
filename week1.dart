4055ca2020e0547b9b0e259dd99e622a2debd7dc
Rihana <rihana25fathima@gmail.com>
2026-07-10 10:10:51 +0530

    first commit

---
+++ week1.dart
@@ -0,0 +1,24 @@
+void CheckNumber(int num){
+  if(num>0){
+    print("positive");
+  }else if(num<0){
+    print("negative");
+  }else{
+    print("zero");
+  }
+}
+//even odd
+void checkEvenodd(int num){
+  if(num%2==0){
+    print("even");
+  }else{
+    print("odd");
+  }
+}
+
+
+void main(){
+  CheckNumber(5);
+}
+
+
